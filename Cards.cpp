42c4f0f6a4709efc07f7e7e511d18603b661aa2c
Giwon <leecode99@gmail.com>
2022-09-29 00:38:36 -0400

    Adding classes and its member functions/variables, not done tho

--- Cards.cpp
+++ Cards.cpp
@@ -1,1 +1,41 @@
-cpp file
+/**
+ 1. All data members of user-defined class type must be of pointer type.
+
+no inline function
+
+4. All classes must implement a correct copy constructor, assignment operator,
+and stream insertion operator.
+
+Assignment operators
+in class
+Employee& operator =(const Employee & e);
+method
+Employee &Employee:: operator(const Employee&e){
+    this -> name = new string(*e(e.name))
+    this -> num = new int(*e(e.num))
+    this -> pay = new double(*e(e.pay))
+    return *this;
+}
+
+copy consturtor e.g
+in class
+Employee(const Employee &e);
+method
+Employee::Employee(const Employee&e){
+    this -> name = new string(*e(e.name))
+    this -> num = new int(*e(e.num))
+    this -> pay = new double(*e(e.pay))
+}
+
+5. Memory leaks must be avoided.
+Use delete
+~A(){};
+
+6. Code must be documented using comments (user-defined classes, methods, free
+functions, operators).
+
+*/
+
+#include "Cards.h"
+
+
--- Cards.h
+++ Cards.h
@@ -1,1 +1,83 @@
-header file
+#pragma once
+
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+using std::ostream;
+using std::string;
+using std::vector;
+
+class Card {
+public:
+  void *play();
+  Card();
+  ~Card();
+
+  // assignment operator
+  Card &operator=(const Card &c);
+
+  // Copy constructor
+  Card(const Card &c);
+
+  // Stream insertion operator
+  friend ostream &operator<<(ostream &out, const Card &Card);
+
+private:
+  // Each card has a type from: bomb, reinforcement, blockade, airlift, and
+  // diplomacy.
+  vector<string> type_card = {"Bomb", "Reinforcement", "Blockade", "Airlift",
+                              " Diplomacy"};
+
+  // Vector gives me a red line saying "
+  // Non-aggregate type 'vector<std::string>'(aka 'vector<basic_string<char,
+  // char_traits<char>, allocator<char> > >') cannot be initialized with an
+  // initializer list"
+  // Seems like a common problem for c++ 11.
+  // You can run the code with  =>      g++ -std=c++ <filename>
+  // Complies with no problem
+};
+
+class Deck : public Card {
+  Card *card;
+
+public:
+  void *draw();
+  Deck();
+  ~Deck();
+  Deck(Card *card);
+  // assignment operator
+  Deck &operator=(const Deck &d);
+
+  // copy constructor
+  Deck(const Deck &d);
+
+  // Stream insertion operator
+  friend ostream &operator<<(ostream &out, const Deck &Deck);
+
+  // Deck will be generated with this method.
+  void genDeck();
+
+private:
+// The deck that will contain cards.
+  vector<Card *> deck;
+};
+
+class Hand {
+
+public:
+  Hand();
+  ~Hand();
+
+  // assignment operator
+  Hand &operator=(const Hand &h);
+
+  // copy constructor
+  Hand(const Hand &h);
+
+  // Stream insertion operator
+  friend ostream &operator<<(ostream &out, const Hand &Hand);
+};
+
+void testCards();
