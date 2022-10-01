fb625fd9487203839b52911d3b1f385167396c80
Chuanqi-123 <michaelmo052517@gmail.com>
2022-10-01 14:50:40 -0400

    added code

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -32,53 +32,83 @@ void GameEngine::setState(string * new_state) {
 
 
 void GameEngine::changeState(string * input) {
-
+  // -------------startup-------------------  
   if (*curr_state == "start") {
-    cout << "Current state is start" << endl;
     if (*input == "loadmap") {
       //loadMap();
-      cout << "Map has been loaded" << endl;
-      setState(new string("loadmap"));
+      setState(new string("map loaded"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
-  }
-
-  if (*curr_state == "loadmap") {
-    cout << "Current state is load map" << endl;
+  } else if (*curr_state == "map loaded") {
     if (*input == "loadmap") {
       //loadMap();
-      cout << "Map has been loaded" << endl;
-      setState(input);
-    }  else if (*input == "validatemap") {
+      setState(new string("map loaded"));
+    } else if (*input == "validatemap") {
       //validateMap();
-      cout << "Map has been validated" << endl;
-      setState(input);
+      setState(new string("map validated"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
-  }
-
-  if (*curr_state == "validatemap") {
-    cout << "Current state is validate map" << endl;
+  } else if (*curr_state == "map validated") {
     if (*input == "addplayer") {
       //addPlayer();
-      cout << "Player has been added" << endl;
-      setState(input);
+      setState(new string("players added"));
+    } else {
+       cout << "You have entered an invalid command" << endl;
+    }
+  } else if (*curr_state == "players added") {
+    if (*input == "addplayer") {
+      //addPlayer();
+      setState(new string("players added"));
+    } else if (*input == "assigncountries") {
+      //assignReinforcement();
+      setState(new string("assign reinforcement"));
+    } else {
+       cout << "You have entered an invalid command" << endl;
+    }
+  // -------------play-------------------  
+  } else if (*curr_state == "assign reinforcement") {
+    if (*input == "issueorder") {
+      //issue_order();
+      setState(new string("issue orders"));
+    } else {
+       cout << "You have entered an invalid command" << endl;
+    }
+  } else if (*curr_state == "issue orders") {
+    if (*input == "issueorder") {
+      //issue_order();
+      setState(new string("issue orders"));
+    } else if (*input == "endissueorders") {
+      //endIssueOrders();
+      setState(new string("execute orders"));
+    } else {
+       cout << "You have entered an invalid command" << endl;
+    }
+  } else if (*curr_state == "execute orders") {
+    if (*input == "execorder") {
+      //execOrder();
+      setState(new string("execute orders"));
+    } else if (*input == "endexecorders") {
+      //assignReinforcement();
+      setState(new string("assign reinforcement"));
+    } else if (*input == "win") {
+      //win();
+      setState(new string("win"));
+    }else {
+       cout << "You have entered an invalid command" << endl;
+    }
+  } else if (*curr_state == "win") {
+    if (*input == "play") {
+      //start();
+      setState(new string("start"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
   }
 
-  if (*curr_state == "addplayer") {
-    if (*input == "assigncountries") {
-      //addPlayer();
-      cout << "Player has been added" << endl;
-      setState(input);
-    } else {
-       cout << "You have entered an invalid command" << endl;
-    }
-  }
+
+
 
 }
 
--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -6,10 +6,18 @@ int main() {
 
     GameEngine engine = GameEngine();
     string input;
+    string last;
+    cout << "---------Please enter new input------------" << endl;
 
-    cout << "Please enter new input" << endl;
     while (cin >> input) {
+        if (! input.compare("end") && ! last.compare("win")) {
+            engine.~GameEngine();
+            cout << "You have ended the game" << endl;
+            break;
+        }
+
         engine.changeState(&input);
-        cout << "Please enter new input" << endl;
+        cout << "---------Please enter new input------------" << endl;
+        last = input;
     }
 }
