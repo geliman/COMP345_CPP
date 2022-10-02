e97e3e4605d1d374e10b9ec44418b15908238784
= <=>
2022-10-02 17:37:37 -0400

    MapDriver creates a data vector containing all the information to create a map. The organization is as follows: vector[continent][                 name,                 points,                 (territories = i>1 and is even -> name;                                       i>2 and is odd -> properties:                                                                     x,y,adjTerr ]
    Next step is to integrate in Map object and implement, so we can callback within mapDriver.
    
    @TO DO : Big three for Map, Big Three for Territory, pointers architecture, validation method with try/catch, memory clear, documentation

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -1,16 +1,16 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
 
-int main() {
+int testLoadMaps() {
 
-    ifstream file("./maps/Grand Montreal.map");
     string str;
     vector<vector<string>> arr_cont; /* [i][0]  -> continents ; [i][n] -> n > 0, Territoires d'un continent 'i' */
-    
+    ifstream file("./maps/Grand Montreal.map");
 
     while (getline(file, str)) {
 
@@ -18,8 +18,17 @@ int main() {
 
             while (getline(file, str) && str != "[Territories]") {
                 if (str != "" && str != "\n") {
+                    int found;
+
+                    string temp2;
                     vector<string> temp;
-                    temp.push_back(str);
+                    string delimiter = "=";
+
+
+                    temp2 = str.substr(0, str.find(delimiter));
+                    temp.push_back(temp2);
+                    temp2 = str.substr(str.find(delimiter) + 1, str.length() - 1); 
+                    temp.push_back(temp2);
                     arr_cont.push_back(temp);
                 }
                 
@@ -32,31 +41,47 @@ int main() {
 
         if (str == "[Territories]"){
             int index = 0;
-            
+            string delimiter2 = ",";
+            string terrName;
+            string terrProperties;
+
                 while (getline(file, str)) {
                     if (str != "" && str != "\n") {
-                        arr_cont.at(index).push_back(str);
+                        terrName = str.substr(0, str.find(delimiter2));
+                        terrProperties = str.substr(str.find(delimiter2) + 1, str.length() - 1);
+                        arr_cont.at(index).push_back(terrName);
+                        arr_cont.at(index).push_back(terrProperties);
+                        
+
                     }
                     else {
                         index++;
                     }
                 }
-            
-            
         }
         for (int i = 0; i < arr_cont.size(); i++) {
 
-            cout << "Continent" << i+1 << " : " << arr_cont.at(i).at(0) << "\n\n";
-
-            for (int j = 1; j < arr_cont.at(i).size(); j++) {
-
-                cout << "Territoire" << j << " : " << arr_cont.at(i).at(j) << "\n";
-
+            cout << "Continent #" << i + 1 << " : " << arr_cont.at(i).at(0) << "\n\n";
+            cout << "Points" << " : " << arr_cont.at(i).at(1) << "\n\n";
+            for (int j = 2; j < arr_cont.at(i).size(); j+=2) {
+                /* j = territory name; j+1 = territory properties */
+                cout << "Territory #" << j-1 << " : " << arr_cont.at(i).at(j) << "\n";
+                cout << "Territory properties :" << arr_cont.at(i).at(j+1) << "\n\n";
             }
-            cout << "\n";
+            
+            cout << "\n******************************\n";
         }
 
     }
-    
+    file.close();
     return 0;
+}
+
+
+int main() {
+    
+    testLoadMaps();
+    cout << "Okay";
+    return 0;
+
 }
