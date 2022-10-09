ea971ca5b0d4fcb3ccf67fced27e8383db09289c
Chuanqi-123 <michaelmo052517@gmail.com>
2022-10-08 21:26:05 -0400

    added comment

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 // constructor
 GameEngine::GameEngine() {
+  //set the state to start when a new game engine is created
   string * str = new string("start");
   curr_state = str;
   cout << "State set to " << * str << endl;
@@ -34,83 +35,71 @@ GameEngine::~GameEngine() {
   curr_state = NULL;
 }
 
-
+// the setter function to set state to a new value
 void GameEngine::setState(string * new_state) {
   delete curr_state;
   curr_state = new_state;
   cout << "State set to " << * new_state << endl;
 }
 
+// changeState takes user entered transition and check if it is a valid input, if so, make the transition
 void GameEngine::changeState(string * input) {
-  // -----------------startup----------------------  
+  // -------------------startup----------------------  
   if (*curr_state == "start") {
     if (*input == "loadmap") {
-      //loadMap();
       setState(new string("map loaded"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
   } else if (*curr_state == "map loaded") {
     if (*input == "loadmap") {
-      //loadMap();
       setState(new string("map loaded"));
     } else if (*input == "validatemap") {
-      //validateMap();
       setState(new string("map validated"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
   } else if (*curr_state == "map validated") {
     if (*input == "addplayer") {
-      //addPlayer();
       setState(new string("players added"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
   } else if (*curr_state == "players added") {
     if (*input == "addplayer") {
-      //addPlayer();
       setState(new string("players added"));
     } else if (*input == "assigncountries") {
-      //assignReinforcement();
       setState(new string("assign reinforcement"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
-  // -------------play-------------------  
+  // ------------------play-------------------  
   } else if (*curr_state == "assign reinforcement") {
     if (*input == "issueorder") {
-      //issue_order();
       setState(new string("issue orders"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
   } else if (*curr_state == "issue orders") {
     if (*input == "issueorder") {
-      //issue_order();
       setState(new string("issue orders"));
     } else if (*input == "endissueorders") {
-      //endIssueOrders();
       setState(new string("execute orders"));
     } else {
        cout << "You have entered an invalid command" << endl;
     }
   } else if (*curr_state == "execute orders") {
     if (*input == "execorder") {
-      //execOrder();
       setState(new string("execute orders"));
     } else if (*input == "endexecorders") {
-      //assignReinforcement();
       setState(new string("assign reinforcement"));
     } else if (*input == "win") {
-      //win();
       setState(new string("win"));
     }else {
        cout << "You have entered an invalid command" << endl;
     }
   } else if (*curr_state == "win") {
     if (*input == "play") {
-      //start();
       setState(new string("start"));
     } else {
        cout << "You have entered an invalid command" << endl;
--- GameEngineDriver.cpp
+++ GameEngineDriver.cpp
@@ -17,8 +17,9 @@ void testGameStates() {
     string last;
     cout << "---------Please enter new input------------" << endl;
 
+    // continously reading input from console
     while (cin >> input) {
-        if (! input.compare("end") && ! last.compare("win")) {
+        if (!input.compare("end") && !last.compare("win")) {
             engine.~GameEngine();
             cout << "You have ended the game" << endl;
             break;
